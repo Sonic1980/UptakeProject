faf36daba8e1148dedbfd13523330be9346f679b
Sonic1980 <r_escamilla1980@live.com>
2016-12-02 08:48:42 -0600

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+
+namespace UptakeAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new MainClass();
+            c.UptakeTest();
+        }
+
+    }
+}
+
